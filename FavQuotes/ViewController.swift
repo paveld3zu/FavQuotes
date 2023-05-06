0597f1acdee98d1090a1e04e8856ee1421ef698d
paveld3zu <pavel.karunnyj@yandex.ru>
2023-05-06 17:29:42 +0300

    Initial Commit

---
+++ FavQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavQuotes
+//
+//  Created by Pavel Karunnyj on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
