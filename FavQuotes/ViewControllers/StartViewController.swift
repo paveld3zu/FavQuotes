63a6b4e92844f435eed965d9591b151fc0cf6049
paveld3zu <pavel.karunnyj@yandex.ru>
2023-05-10 23:43:40 +0300

    Fix function

--- FavQuotes/ViewControllers/StartViewController.swift
+++ FavQuotes/ViewControllers/StartViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 final class StartViewController: UIViewController {
 
     var quote: Quote!
-
-    @IBAction func getQuoteButtonTapped(_ sender: UIButton) {
-    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let quoteVC = segue.destination as? QuoteViewController else { return }
         quoteVC.quote = quote
+    }
+
+    @IBAction func getQuoteButtonTapped(_ sender: UIButton) {
     }
 }
